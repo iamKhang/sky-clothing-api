3993bcf19b310e39d9228d2efbf4adc7283945a0
iamKhangg <iamhoangkhang@icloud.com>
2024-12-27 21:51:26 +0700

    Khởi tạo dự án

---
+++ src/main/java/com/iamkhangg/skyclothingapi/SkyClothingApiApplication.java
@@ -0,0 +1,13 @@
+package com.iamkhangg.skyclothingapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyClothingApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkyClothingApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/iamkhangg/skyclothingapi/SkyClothingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iamkhangg.skyclothingapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyClothingApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
